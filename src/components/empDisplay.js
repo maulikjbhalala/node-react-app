73c1c9475661f8fe723b477bd3fc686f4a40429f
maulik.j.bhalala <maulik.j.bhalala@doyenhub.com>
2020-07-23 17:43:11 +0530

    loading  component button while get data and functional component adding

--- src/components/empDisplay.js
+++ src/components/empDisplay.js
@@ -10,9 +10,12 @@ import EditIcon from '@material-ui/icons/Edit';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 // import AddIcon from '@material-ui/icons/Add';
 // import Loader from './loader';
+// import Loader from 'react-loader-spinner'
+import LoadComponent from './dataLoader'
 
-
-
+import LoadingSpinner from './dataLoader'
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Display extends React.Component {
 
@@ -22,72 +25,85 @@ class Display extends React.Component {
         this.state =
         {
             emps: [],
-            loading: true
+            loading: false
         };
+        this.onClick=this.onClick.bind(this)
     }
 
-    componentWillMount() {
-        this.getDataFromAPI();
-    };
+    // componentWillMount() {
+    //     this.getDataFromAPI();
+    // };
 
-
-    getDataFromAPI() {
-
+    onClick = () => {
+        this.setState({
+            loading:true
+        })
         axios.get('http://localhost:5600/emp')
             .then(res => {
                 this.setState({ emps: res.data, loading: false });
             });
-    };
+      }
+
 
   
  
 
     render() {
-   
-        return (
-            <div className="container">
-                <div className="panel panel-default">
-                    <div className="panel-heading">
-                        <h3 className="panel-title">
-                            <center style={{ color: 'Blue' }}>Show Employees</center>
-                        </h3>
-                    </div>
-                    <div>
-           
-           </div>
-                    <div className="panel-body">
-                        <h4><Link to="/add"><span  class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span> Add Employee</Link></h4>
-                        <table padding='2' class="table table-stripe">
-                            <thead>
-                                <tr style={{ color: 'DarkOrchid' }}>
-                                    <th>Emp Name</th>
-                                    <th>Emp Email</th>
-                                    <th><VisibilityIcon style={{ fontSize: 22 }} /> </th>
-                                    <th><EditIcon style={{ fontSize: 22 }} /> </th>
-                                     <th><DeleteIcon style={{ fontSize: 22 }} /></th>
-                                </tr>
-                                <p></p>
-                            </thead>
-                            <tbody>
 
-                                { this.state.emps.map(emp =>
-                                    <tr style={{ color: 'brown' }} key={emp._id} >
-                                        <td>{emp.empName}</td>
-                                        <td >{emp.empEmail}</td>
-                                       <td><Link to={'/viewById/'+emp._id}>View More</Link></td>
-                                       <td><Link to={'/edit/'+emp._id}>Edit</Link></td>
-                                       <td><Link to={'/delete/'+emp._id}>Delete</Link></td>
+            return (
+                <div className="container">
+                      <div className="panel-body">
+                      <div  className="panel-body">
+                <Button variant="outline-success" onClick={this.onClick}>Fetch Data</Button>
+                    </div>
+                      </div>
+                   
+                    
+            {this.state.loading?<LoadingSpinner/>: <div className="panel panel-default">
+                        <div className="panel-heading">
+                            <h3 className="panel-title">
+                                <center style={{ color: 'Blue' }}>Show Employees</center>
+                            </h3>
+                        </div>
+                        <div>
+               
+               </div>
+                        <div className="panel-body">
+                            <h4><Link to="/add"><span  class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span> Add Employee</Link></h4>
+                            <table padding='2' class="table table-stripe">
+                                <thead>
+                                    <tr style={{ color: 'DarkOrchid' }}>
+                                        <th>Emp Name</th>
+                                        <th>Emp Email</th>
+                                        <th><VisibilityIcon style={{ fontSize: 22 }} /> </th>
+                                        <th><EditIcon style={{ fontSize: 22 }} /> </th>
+                                         <th><DeleteIcon style={{ fontSize: 22 }} /></th>
                                     </tr>
-                                )}
-                            </tbody>
-                        </table>
-                    </div>
+                                    <p></p>
+                                </thead>
+                                <tbody>
+    
+                                    { this.state.emps.map(emp =>
+                                        <tr style={{ color: 'brown' }} key={emp._id} >
+                                            <td>{emp.empName}</td>
+                                            <td >{emp.empEmail}</td>
+                                           <td><Link to={'/viewById/'+emp._id}>View More</Link></td>
+                                           <td><Link to={'/edit/'+emp._id}>Edit</Link></td>
+                                           <td><Link to={'/delete/'+emp._id}>Delete</Link></td>
+                                        </tr>
+                                    )}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>}
+                   
+              
+               
                 </div>
-          
-           
-            </div>
-
-        )
+    
+            )
+        // }
+       
     };
 };
 
--- src/components/mountGetFunctional.js
+++ src/components/mountGetFunctional.js
@@ -1,77 +1,100 @@
-import Axios from 'axios';
+// import Axios from 'axios';
 
-// import 
-import React, { useEffect, useState } from 'react';
-
-
-
-const GetAPICall =()=>
-{
-    const [name,setName]=useState('');
-    const [count,setCount]=useState(0);
-    const [data,setData]=useState([]);
-
-    const dataChange=(e)=>
-    {
-        setName(e.target.value)
-    };
+// // import 
+// import React, { useEffect, useState } from 'react';
 
 
 
+// const GetAPICall =()=>
+// {
+//     const [name,setName]=useState('');
+//     const [count,setCount]=useState(0);
+//     const [data,setData]=useState([]);
 
-    // useEffect(()=>
-    // {
-    //     setName('mau',[])
-    //     // setCount(count+1)
-
-    // }); 
-
-    useEffect(()=>
-    {
-        setData([]);
-       Axios.get('http://localhost:5600/d').then((result)=>
-       {
-        console.log('result is',result)
-        setData(result.data.depts);
-       })
-       
-    })
+//     const dataChange=(e)=>
+//     {
+//         setName(e.target.value)
+//     };
 
 
-
-    const incrementCount=()=>
-    {
-        setCount(count+1)
-    };
 
     
 
 
-    return(
-        <div>
-            <h1>input form:</h1>
-            <form >
-                <p>name:</p>
-                <input type='text' id='name'  onChange={dataChange}></input>
-                <button type='button' onClick={incrementCount}>increment</button>
-            </form>
-            <div >
-                name is <h3 style={{color:'brown'}}>{name}</h3>
-                count <h3>{count}</h3>
-                </div>
-                <div>
-                    {console.log('array',data)}
-                    {data.map((dataObj)=>
-                    
-                        // {console.log('data',dataObj)}
-                        <h1>{dataObj}</h1>
-
-                    )}
-                </div>
-        </div>
-    )
-
-};
 
 
-export default GetAPICall;
+    
+// useEffect(() => {  
+//     const GetData = async () => {  
+//       const result = await axios('http://localhost:5600/d');  
+//       setData(result.data.depts);  
+//     };  
+  
+//     GetData();  
+//   }, []); 
+
+
+//     const incrementCount=()=>
+//     {
+//         setCount(count+1)
+//     };
+
+
+//     return(
+//         <div>
+//             <h1>input form:</h1>
+//             <form >
+//                 <p>name:</p>
+//                 <input type='text' id='name'  onChange={dataChange}></input>
+//                 <button type='button' onClick={incrementCount}>increment</button>
+//             </form>
+//             <div >
+//                 name is <h3 style={{color:'brown'}}>{name}</h3>
+//                 count <h3>{count}</h3>
+//                 </div>
+//                  {/* <div className="panel-body">
+//                         <table padding='2' class="table table-stripe">
+//                             <thead>
+//                                 <tr style={{ color: 'DarkOrchid' }}>
+//                                     <th>Emp Name</th>
+                                  
+//                                 </tr>
+//                                 <p></p>
+//                             </thead>
+//                             <tbody>
+
+//                                 { data.map(emp =>
+//                                     <tr style={{ color: 'brown' }} >
+//                                         <td>{emp}</td>
+//                                     </tr>
+//                                 )}
+//                             </tbody>
+//                         </table>
+//                     </div> */}
+//         </div>
+//     )
+
+
+
+
+//      //Define State
+//     //  const [name, setName] = useState({firstName: 'name', surname: 'surname'});
+//     //  const [title, setTitle] = useState('BIO');
+    
+//     //  //Call the use effect hook
+//     //  useEffect(() => {
+//     //    setName({FirstName: 'Shedrack', surname: 'Akintayo'})
+//     //    setTitle('mote')
+//     //  }, [])//pass in an empty array as a second argument
+     
+//     //  return(
+//     //      <div>
+//     //          <h1>Title: {title}</h1>
+//     //          <h3>Name: {name.firstName}</h3>
+//     //          <h3>Surname: {name.surname}</h3>
+//     //      </div>
+//     //  );
+// };
+
+
+// export default GetAPICall;
